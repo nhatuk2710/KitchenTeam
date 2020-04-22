97c2f953c295ffed993be3c3569276f0b4f33102
nhatuk2710 <49880246+nhatuk2710@users.noreply.github.com>
2020-04-23 03:17:11 +0700

    code dinh

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+@includeIf('html.head')
+<body class="animsition">
+
+<!-- header fixed -->
+@includeIf('html.header')
+
+<!-- Slide1 -->
+
+
+<!-- Banner -->
+@yield('all')
+
+
+<!-- Footer -->
+@includeIf('html.footer')
+
+<!--===============================================================================================-->
+@includeIf('html.script')
+
+</body>
+</html>
